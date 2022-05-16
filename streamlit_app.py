a8fe6d4577c64511d8d01d36098d3e9f365e6a0f
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-05-16 03:17:00 +0200

    remove design file

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 import os
 
-# import design augmentation for streamlit UX/UI
-import streamlit_design as streamlit_design
-
 # custom component for recording client audio in browser
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
@@ -17,12 +14,24 @@ st_audiorec = components.declare_component("st_audiorec", path=build_dir)
 def audiorec_demo_app():
 
     # DESIGN implement changes to the standard streamlit UI/UX
-    streamlit_design.design_setup()  # switch to primaryColor
+
+    # Design page layout and browser window details
+    st.set_page_config(layout="centered",
+                       page_title="streamlit_audio_recorder")
+
+    # Design move app further up and remove top padding
+    st.markdown('''<style>.css-1egvi7u {margin-top: -8rem;}</style>''',
+        unsafe_allow_html=True)
+
+    # Design change st.Audio to fixed height of 45 pixels
+    st.markdown('''<style>.stAudio {height: 45px;}</style>''',
+        unsafe_allow_html=True)
 
     st.title('streamlit audio recorder')
     st.markdown('Version 1.3.2 - April 2022 - '
         '[@GitHub](https://github.com/stefanrmmr/streamlit_audio_recorder)')
 
+    # CREATE INSTANCE of "Streamlit Audio Recorder" - by stefanrmmr
     st_audiorec()
 
 
