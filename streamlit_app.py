4d43567155655b8a58c6c18d16720df84dc4fd4b
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-05-16 04:42:04 +0200

    final touches

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,11 @@ def audiorec_demo_app():
     # DESIGN implement changes to the standard streamlit UI/UX
     st.set_page_config(page_title="streamlit_audio_recorder")
     # Design move app further up and remove top padding
-    st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''',
-        unsafe_allow_html=True)
+    st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''', unsafe_allow_html=True)
+    # Design change hyperlink href link color
+    st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''', unsafe_allow_html=True)
     # Design change st.Audio to fixed height of 45 pixels
-    st.markdown('''<style>.stAudio {height: 45px;}</style>''',
-        unsafe_allow_html=True)
+    st.markdown('''<style>.stAudio {height: 45px;}</style>''', unsafe_allow_html=True)
 
     # TITLE and Creator information
     st.title('streamlit audio recorder')
