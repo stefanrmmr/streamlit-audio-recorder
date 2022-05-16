0f2ff7fab48f11b88c941665449f01e293dde5f8
Stefan Rummer <82606558+stefanrmmr@users.noreply.github.com>
2022-05-16 04:44:39 +0200

    final touches

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import streamlit.components.v1 as components
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 
 # LIGHTMODE custom component for recording client audio in browser
+# (alternatively there is also source code provided for darkmode design)
 build_dir_light = os.path.join(parent_dir, "st_audiorec_light/frontend/build")
 # specify directory and initialize st_audiorec object functionality
 st_audiorec_light = components.declare_component("st_audiorec", path=build_dir_light)
@@ -17,11 +18,14 @@ def audiorec_demo_app():
     # DESIGN implement changes to the standard streamlit UI/UX
     st.set_page_config(page_title="streamlit_audio_recorder")
     # Design move app further up and remove top padding
-    st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''', unsafe_allow_html=True)
-    # Design change hyperlink href link color
-    st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''', unsafe_allow_html=True)
+    st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''',
+        unsafe_allow_html=True)
     # Design change st.Audio to fixed height of 45 pixels
-    st.markdown('''<style>.stAudio {height: 45px;}</style>''', unsafe_allow_html=True)
+    st.markdown('''<style>.stAudio {height: 45px;}</style>''',
+        unsafe_allow_html=True)
+    # Design change hyperlink href link color
+    st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''',
+        unsafe_allow_html=True)
 
     # TITLE and Creator information
     st.title('streamlit audio recorder')
@@ -34,8 +38,8 @@ def audiorec_demo_app():
     # COLUMNS for custom alignment
     audio_col0_, audio_col1_ = st.columns([1, 0.5])
     with audio_col0_:
-        # STREAMLIT AUDIO RECORDER Instance - Lightmode
-        st_audiorec_light(key="recorder_light")
+        # STREAMLIT AUDIO RECORDER Instance
+        st_audiorec_light()
 
 
 if __name__ == '__main__':
