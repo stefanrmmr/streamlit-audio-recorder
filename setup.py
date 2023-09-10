a0295e5c8da22ae374424aba48006dd045b1a741
Stefan Rummer <tenderport.info@gmail.com>
2023-09-11 01:21:17 +0200

    updated pypi setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-audiorec",
-    version="0.0.5",
+    version="0.0.7",
     author="Stefan Rummer",
     author_email="stefan.rummer@outlook.com",
     description="Record audio from the user's microphone in apps that are deployed to the web. (via Browser Media-API) [GitHub ☆ 160+: steamlit-audio-recorder]",
--- st_audiorec/__init__.py
+++ st_audiorec/__init__.py
@@ -4,12 +4,13 @@ import streamlit as st
 from io import BytesIO
 import streamlit.components.v1 as components
 
+
 def streamlit_audio_recorder():
 
     # get parent directory relative to current directory
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     # Custom REACT-based component for recording client audio in browser
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
+    build_dir = os.path.join(parent_dir, "frontend/build")
     # specify directory and initialize st_audiorec object functionality
     st_audiorec = components.declare_component("st_audiorec", path=build_dir)
 
